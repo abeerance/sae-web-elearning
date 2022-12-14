4edf71759a751a3d72c48a55d327cffae11d5f6b
Hadrian Chio <hadrian.chio@openinteractive.ch>
2022-12-14 13:48:56 +0100

    succesfully called GraphQL Api

--- common/data/apiClient.ts
+++
@@ -1,4 +0,0 @@
-export const graphqlApi = process.env.HYGRAPH_PROJECT_API;
-export const headers = {
-  'content-type': 'application/json',
-};
--- pages/javascript/[id].tsx
+++ pages/javascript/[id].tsx
@@ -1,7 +1,29 @@
-import request, { gql } from 'graphql-request';
-import { useEffect, useState } from 'react';
+import { gql } from '@apollo/client';
+import apolloClient from '../../apollo-client';
 
-export default function JavaScriptModule() {
+export async function getServerSideProps() {
+  const { data } = await apolloClient.query({
+    query: gql`
+      query JavaScriptModules {
+        javaScriptModules {
+          module
+          content {
+            html
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      modules: data.javaScriptModules[0],
+    },
+  };
+}
+
+export default function JavaScriptModule({ modules }: { modules: {} }) {
+  console.log(modules);
   return (
     <>
       <></>
