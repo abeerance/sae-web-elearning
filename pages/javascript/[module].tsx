14920f54dec7887cd975c2bf4c635687bc310d1b
Hadrian Chio <hadrian.chio@openinteractive.ch>
2022-12-15 10:58:27 +0100

    implemented dynamic richtextrender, except buttons

--- pages/javascript/[module].tsx
+++ pages/javascript/[module].tsx
@@ -3,8 +3,7 @@ import { RichText } from '@graphcms/rich-text-react-renderer';
 import { RichTextContent } from '@graphcms/rich-text-types';
 import { Box, Typography } from '@mui/material';
 import apolloClient from '../../apollo-client';
-import Tiptap from '../../common/components/common/wysiwyg/tiptap';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 
 export async function getServerSideProps() {
   const { data } = await apolloClient.query({
@@ -22,22 +21,20 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      modules: data.javaScriptModules[0],
       content: data.javaScriptModules[0].content.json,
     },
   };
 }
 
 export default function JavaScriptModule({
-  modules,
   content,
 }: {
-  modules: {};
   content: RichTextContent;
 }) {
   console.log(content);
   return (
     <Box
+      className="content-container"
       sx={{
         display: 'flex',
         flexDirection: 'column',
@@ -62,26 +59,46 @@ export default function JavaScriptModule({
             <Box sx={{ width: '100%' }}>
               <Typography
                 variant="h2"
-                sx={{ margin: '5rem 0 2rem 0', textAlign: 'left' }}
+                sx={{ margin: '5rem 0 1rem 0', textAlign: 'left' }}
               >
                 {children}
               </Typography>
             </Box>
           ),
           p: ({ children }) => (
-            <Box sx={{ width: '100%' }}>
-              <Typography
-                sx={{
-                  margin: '1rem 0',
-                  lineHeight: '3rem',
-                  fontSize:
-                    'calc(18px + (20 - 18) * ((100vw - 300px) / (1600 - 300)))',
-                  textAlign: 'left',
-                }}
-              >
-                {children}
-              </Typography>
-            </Box>
+            <Typography
+              sx={{
+                margin: '1rem 0',
+                lineHeight: '3rem',
+                fontSize:
+                  'calc(18px + (20 - 18) * ((100vw - 300px) / (1600 - 300)))',
+                textAlign: 'left',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              {children}
+            </Typography>
+          ),
+          a: ({ children, openInNewTab, href, rel }) => (
+            <Typography
+              className="test"
+              component="a"
+              sx={{
+                background: '#F58700',
+                fontSize: `calc(18px + (20 - 18) * ((100vw - 300px) / (1600 - 300)))`,
+                padding: '1.8rem 4rem',
+                margin: '5rem auto',
+                borderRadius: '10px',
+                position: 'relative',
+              }}
+              href={href}
+              target={openInNewTab ? '_blank' : '_self'}
+              rel={rel || 'noopener noreferrer'}
+            >
+              {children}
+            </Typography>
           ),
           // @ts-expect-error: TODO
           img: ({
@@ -102,6 +119,8 @@ export default function JavaScriptModule({
                 borderRadius: '10px',
                 position: 'relative',
                 overflow: 'hidden',
+                marginTop: '5rem',
+                marginBottom: '5rem',
               }}
             >
               <Image src={src} alt={altText} fill quality={100} />
