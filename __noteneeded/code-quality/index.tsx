a49525a21378e8cb7db03636509b4bd51ff93c3c
Hadrian Chio <hadrian.chio@openinteractive.ch>
2022-12-15 15:50:11 +0100

    adjusted Paragraph

--- __noteneeded/code-quality/index.tsx
+++
@@ -1,9 +0,0 @@
-import Tiptap from '../../../common/components/common/wysiwyg/tiptap';
-
-export default function CodeQuality() {
-  return (
-    <>
-      <Tiptap content="<h1>asda</h1>" />
-    </>
-  );
-}
